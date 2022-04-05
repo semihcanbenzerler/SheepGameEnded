f075827987b3aaa727ac076323410948fca3cc38
semihcanbenzerler <semih.benzerler@gmail.com>
2022-04-05 12:44:04 +0300

    Initial commit

---
+++ Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float movementSpeed;
+
+    public float horizontalBoundary = 22;
+
+    public GameObject hayBalePrefab; 
+    public Transform haySpawnpoint; 
+    public float shootInterval; 
+    private float shootTimer;
+    public Transform modelParent;
+
+
+    public GameObject blueModelPrefab;
+    public GameObject yellowModelPrefab;
+    public GameObject redModelPrefab;
+
+    private void Start()
+    {
+        LoadModel();
+    }
+    private void LoadModel()
+    {
+        Destroy(modelParent.GetChild(0).gameObject); // 1
+
+        switch (GameSettings.hayMachineColor) // 2
+        {
+            case HayMachineColor.Blue:
+                Instantiate(blueModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Yellow:
+                Instantiate(yellowModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Red:
+                Instantiate(redModelPrefab, modelParent);
+                break;
+        }
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
+
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) // 1
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) // 2
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+}
---
+++ Assets/RW/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager Instance;
+
+    public AudioClip shootClip;
+    public AudioClip sheepHitClip;
+    public AudioClip sheepDroppedClip;
+
+    private Vector3 cameraPosition;
+
+   
+    // Start is called before the first frame update
+    void Avake()
+    {
+        Instance = this;
+        cameraPosition = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    private void PlaySound(AudioClip clip)
+
+    {
+        AudioSource.PlayClipAtPoint(clip, cameraPosition);
+    }
+
+    public void PlayShootClip()
+    {
+        PlaySound(shootClip);
+    }
+    public void PlaySheepHitClip()
+    {
+        PlaySound(sheepHitClip);
+    }
+    public void PlaySheepDroppedClip()
+    {
+        PlaySound(sheepDroppedClip);
+    }
+}
